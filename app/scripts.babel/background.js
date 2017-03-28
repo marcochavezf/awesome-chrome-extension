97134a2c395098c891670517c47c4d1e45f05d53
Marco Chávez <marcochavezf@hotmail.com>
2017-03-28 09:10:48 -0600

    Identify app/src and vendor/lib folders

--- app/scripts.babel/background.js
+++ app/scripts.babel/background.js
@@ -1,16 +1,4 @@
 'use strict';
-/*
- chrome.runtime.onInstalled.addListener(function (details) {
- console.log('previousVersion', details.previousVersion);
- });
-
- chrome.tabs.onUpdated.addListener(function (tabId) {
- chrome.pageAction.show(tabId);
- });
-
- console.log('\'Allo \'Allo! Event Page for Page Action');
- */
-
 // Copyright (c) 2011 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
@@ -25,6 +13,9 @@ chrome.debugger.onDetach.addListener(onDetach);
 chrome.browserAction.onClicked.addListener(function(tab) {
   var tabId = tab.id;
   if (!tabsContent[tabId]) {
+    chrome.browserAction.setIcon({tabId: tabId, path:'images/debuggerPausing.png'});
+    chrome.browserAction.setTitle({tabId: tabId, title:'Pausing JavaScript'});
+
     chrome.tabs.sendMessage(tab.id, {action: 'get_tab_content'});
   } else {
     toggleDebugger(tabId);
@@ -35,13 +26,76 @@ chrome.runtime.onMessage.addListener(function(request, sender) {
   var tabId = sender.tab.id;
   if (request.type == 'tab_content') {
     tabsContent[tabId] = request.tabContent;
-    var scriptsContent = request.tabContent.scriptsContent;
-    var paths = _.map(scriptsContent, (content)=>{
-      return content.path;
-    });
+    var projectStructure = getProjectStructure(request.tabContent);
+    //TODO: parse this structure
+    debugger;
     toggleDebugger(tabId);
   }
 });
+
+function getProjectStructure(tabContent) {
+  var scriptsContent = tabContent.scriptsContent;
+  var contentOnlyJs = _.filter(scriptsContent, (content) => {
+    return content.path.includes('.js');
+  });
+  var contentGroupedByLevel = _.groupBy(contentOnlyJs, (content) => {
+    return content.path.split('/').length;
+  });
+
+  var shortestLevel = 0;
+  var largestLevel = 0;
+  _.forIn(contentGroupedByLevel, function(value, key) {
+    var level = parseInt(key);
+    if (shortestLevel) {
+      if (level < shortestLevel) {
+        shortestLevel = level;
+      }
+    } else {
+      shortestLevel = level;
+    }
+
+    if (level > largestLevel) {
+      largestLevel = level;
+    }
+  });
+
+  var angularContentWithShortestPath = null;
+  for (var index = shortestLevel; index <= largestLevel; index++) {
+    var scriptsContentByLevel = contentGroupedByLevel[index];
+    var fileWithAngular = _.find(scriptsContentByLevel, (content) => {
+      var isAngularFile = content.content.includes('angular.module');
+      var isGoogleProperty = content.content.includes('(c) 2010-2015 Google, Inc.');
+      return isAngularFile && !isGoogleProperty
+    });
+    if (fileWithAngular) {
+      angularContentWithShortestPath = fileWithAngular;
+      break;
+    }
+  }
+
+  if (!angularContentWithShortestPath) {
+    throw new Error('There\'s no Angular path!');
+  }
+
+  //TODO: we could ask if this is the project path
+
+  var pathArrayProjectFile = angularContentWithShortestPath.path.split('/');
+  var srcFolder = pathArrayProjectFile[0];
+  if (_.isEmpty(srcFolder)) {
+    srcFolder = pathArrayProjectFile[1];
+  }
+  var srcContent = [];
+  var thirdPartyContent = [];
+  scriptsContent.forEach(content => {
+    if (content.path.includes(srcFolder)) {
+      srcContent.push(content);
+    } else {
+      thirdPartyContent.push(content);
+    }
+  });
+
+  return { srcContent, thirdPartyContent };
+}
 
 function toggleDebugger(tabId){
   var debuggeeId = {tabId:tabId};
