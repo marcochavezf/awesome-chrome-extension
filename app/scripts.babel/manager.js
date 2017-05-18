10a256824154a9f5464333b0921795a1a86a9267
Marco Chávez <marcochavezf@hotmail.com>
2017-05-18 12:13:44 -0500

    add jstree to manager

--- app/scripts.babel/manager.js
+++ app/scripts.babel/manager.js
@@ -28,4 +28,19 @@ function displayContent(content, tabContent){
 }
 
 document.addEventListener('DOMContentLoaded', function() {
+  $(function () {
+    // 6 create an instance when the DOM is ready
+    $('#jstree').jstree();
+    // 7 bind to events triggered on the tree
+    $('#jstree').on('changed.jstree', function (e, data) {
+      console.log(data.selected);
+    });
+    // 8 interact with the tree - either way is OK
+    $('button').on('click', function () {
+      debugger;
+      $('#jstree').jstree(true).select_node('child_node_1');
+      $('#jstree').jstree('select_node', 'child_node_1');
+      $.jstree.reference('#jstree').select_node('child_node_1');
+    });
+  });
 });
